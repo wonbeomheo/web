78b08a4a43d1b9890cae8c1ecc40b55c88f6d45b
Won Beom Heo <wonbeom.heo88@gmail.com>
2024-06-26 10:34:21 +0900

    chore: Database configuration
    set an environment variable for MongoDB URI
    configure the database connection on index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,16 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+    .connect(process.env.MONGODB_URI as string)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log(`Error connecting: ${error}`);
+    });
 
 const app = express();
 
