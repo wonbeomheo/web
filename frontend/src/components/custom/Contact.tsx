4eb02676ab7f722bba55bd300f7b8a678dd50425
Won Beom Heo <wonbeom.heo88@gmail.com>
2024-07-03 18:27:18 +0900

    feat: add a basic layout of the contact section

--- frontend/src/components/custom/Contact.tsx
+++ frontend/src/components/custom/Contact.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui
 
 const Contact = () => {
     return (
-        <div className="p-10 w-screen h-screen bg-black">
+        <div id="contact" className="p-10 w-screen h-screen bg-black">
             <Card className="h-full border-0 bg-gradient-to-b from-black/70 to-black/90 text-white font-oswald">
                 <CardHeader>
                     <CardTitle className="text-6xl">Contact</CardTitle>
@@ -12,13 +12,17 @@ const Contact = () => {
                 <CardContent>
                     <form className="flex flex-col">
                         <label htmlFor="name" className="my-2 mr-4">Name</label>
-                        <input id="name" type="text" className="my-2 rounded-sm bg-gray-400 border-0 outline-red-800 outline-1"></input>
+                        <input id="name" type="text" className="my-2 rounded-sm bg-gray-300 border-0 outline-red-800 outline-1"></input>
                         <label htmlFor="email" className="my-2 mr-4">Email</label>
-                        <input id="email" type="text" className="my-2 rounded-sm bg-gray-400 border-0 outline-red-800 outline-1"></input>
+                        <input id="email" type="text" className="my-2 rounded-sm bg-gray-300 border-0 outline-red-800 outline-1"></input>
                         <label htmlFor="content" className="my-2">Content</label>
-                        <textarea id="content" className="rounded-sm h-screen bg-gray-400"></textarea>
+                        <textarea id="content" className="rounded-sm h-[30vh] bg-gray-300"></textarea>
                     </form>
                     <Button className="mt-4">Submit</Button>
+                    <div className="mt-4">
+                        <a href="https://www.github.com/wonbeomheo">GitHub</a>
+                        <a href="https://www.linkedin.com/in/wonbeom-heo-ryan">LinkedIn</a>
+                    </div>
                 </CardContent>
             </Card>
         </div>
